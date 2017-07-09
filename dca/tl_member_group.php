1e6f20bd7f003ffdd91788539251daf2bd167145
Basti Buck <mail@bastibuck.de>
2017-07-09 18:13:46 +0200

    add fields to member_group

--- dca/tl_member_group.php
+++ dca/tl_member_group.php
@@ -8,3 +8,52 @@
  */
 
 namespace Bastibuck;
+
+// Palettes
+$GLOBALS['TL_DCA']['tl_member_group']['palettes']['default'] = str_replace (
+  '{redirect_legend:hide},redirect;',
+  '{redirect_legend:hide},redirect;{force_pass_change_legend},force_pass_change;',
+  $GLOBALS['TL_DCA']['tl_member_group']['palettes']['default']
+);
+
+
+// Selectors
+$GLOBALS['TL_DCA']['tl_member_group']['palettes']['__selector__'][] = 'force_pass_change';
+
+// Subpalettes
+$GLOBALS['TL_DCA']['tl_member_group']['subpalettes']['force_pass_change'] = 'pass_change_jumpTo';
+
+
+// Fields
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['force_pass_change'] = array
+(
+  'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['force_pass_change'],
+  'exclude'                 => true,
+  'inputType'               => 'checkbox',
+  'eval'                    => array
+  (
+    'submitOnChange'    => true,
+    'tl_class'          => 'w50'
+  ),
+  'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['pass_change_jumpTo'] = array
+(
+  'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['pass_change_jumpTo'],
+  'exclude'                 => true,
+  'inputType'               => 'pageTree',
+  'foreignKey'              => 'tl_page.title',
+  'eval'                    => array
+  (
+    'mandatory'         => true,
+    'fieldType'         => 'radio',
+    'tl_class'          => 'w50'
+  ),
+  'sql'                     => "int(10) unsigned NOT NULL default '0'",
+  'relation'                => array
+  (
+    'type'              => 'hasOne',
+    'load'              => 'eager'
+  )
+);
--- languages/de/tl_member_group.php
+++ languages/de/tl_member_group.php
@@ -8,5 +8,8 @@
  */
 
 // Legends
+$GLOBALS['TL_LANG']['tl_member_group']['force_pass_change_legend'] = 'Passwortänderung';
 
 // Fields
+$GLOBALS['TL_LANG']['tl_member_group']['force_pass_change'] = array('Passwortänderung erzwingen', 'Setzen Sie dieses Häkchen, wenn ein Mitglied dieser Gruppe vor dem Benutzen der Seite zunächst das Passwort ändern muss.');
+$GLOBALS['TL_LANG']['tl_member_group']['pass_change_jumpTo'] = array('Passwortänderungsseite', 'Wählen Sie die Weiterleitungsseite für die Passwortänderung. Auf dieser Seite sollte nur das Passwort-Ändernung-Modul eingebunden werden.');
